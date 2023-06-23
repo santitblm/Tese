6c89896c2a7c5c03d9b81efca3e515958408f82f
santitblm <95491383+santitblm@users.noreply.github.com>
2023-06-23 20:30:21 +0100

    class_distribution

---
+++ RandomCodigos/Crop.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+
+# Define the directory paths
+image_dir = "C:/Users/Santi LM/Downloads/train/images"
+label_dir = "C:/Users/Santi LM/Downloads/train/labels"
+cropped_img_dir = "C:/Users/Santi LM/Downloads/train/images/cropped"
+
+# Get a list of image filenames
+image_files = os.listdir(image_dir)
+
+# Iterate through the image files
+for image_file in image_files:
+    # Check if it's an image file
+    if image_file.endswith(".jpg") or image_file.endswith(".png"):
+        image_name = os.path.splitext(image_file)[0]
+        label_file = os.path.join(label_dir, f"{image_name}.txt")
+
+        # Check if the label file exists and is not empty
+        if os.path.isfile(label_file) and os.path.getsize(label_file) > 0:
+            with open(label_file, "r") as f:
+                lines = f.readlines()
+
+            # Open the original image to get its dimensions
+            image_path = os.path.join(image_dir, image_file)
+            img = cv2.imread(image_path)
+            img_height, img_width, _ = img.shape
+
+            # Iterate through the lines in the label file
+            for i, line in enumerate(lines):
+                line = line.strip().split()
+
+                # Extract the relevant coordinates (scaled to the original image)
+                x = float(line[1]) * img_width
+                y = float(line[2]) * img_height
+                width = float(line[3]) * img_width
+                height = float(line[4]) * img_height
+
+                # Calculate the cropping coordinates
+                left = int(x - width / 2)
+                top = int(y - height / 2)
+                right = int(x + width / 2)
+                bottom = int(y + height / 2)
+
+                # Crop the image
+                cropped_img = img[top:bottom, left:right]
+
+                # Save the cropped image
+                cropped_img_name = f"{i}_{image_name}.jpg"
+                cropped_img_path = os.path.join(cropped_img_dir, cropped_img_name)
+                cv2.imwrite(cropped_img_path, cropped_img)
+                print(f"Saved cropped image: {cropped_img_name}")
+
+print("Image cropping completed.")
---
+++ RandomCodigos/class_distribution.py
@@ -0,0 +1,74 @@
+import os
+import re
+import matplotlib.pyplot as plt
+
+def extract_first_number(line):
+    # Use regular expression to extract the first number from a line
+    match = re.match(r"^(\d+)", line)
+    if match:
+        return int(match.group(1))
+    else:
+        return None
+
+def get_label(number):
+    # Map numbers 0-34 to letters A-Z and digits 0-9
+    labels = 'ABCDEFGHIJKLMNOPQRSTUVXZ0123456789'
+    if 0 <= number <= 34:
+        return labels[number]
+    else:
+        return None
+
+n = 34
+
+def process_text_files(directory):
+    number_counts = [0] * n  # Initialize count for numbers 0 to 33
+
+    # Iterate through each file in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(directory, filename)
+
+            # Read lines from the file
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+
+            # Extract first number from each line and update count
+            for line in lines:
+                number = extract_first_number(line)
+                if number is not None and 0 <= number <= n - 1:
+                    number_counts[number] += 1
+
+    return number_counts
+
+def create_histogram(number_counts):
+    numbers = range(n)
+    frequencies = number_counts
+
+    # Create the histogram
+    bars = plt.bar(numbers, frequencies)
+    plt.xlabel('Numbers')
+    plt.ylabel('Frequency')
+    plt.title('Number Frequency Histogram')
+
+    # Add count labels with letters/digits vertically aligned with y-axis
+    for i, frequency in enumerate(frequencies):
+        if frequency > 0:
+            label = get_label(i)
+            if label is not None:
+                label = label + '_' + str(frequency)
+            else:
+                label = str(frequency)
+            plt.text(i, frequency, label, ha='center', va='bottom', rotation='vertical')
+
+    # Highlight the last 10 bars in red
+    for i in range(n - 10, n):
+        bars[i].set_color('red')
+
+    plt.show()
+
+# Specify the directory containing the text files
+directory = 'C:/Users/Santi LM/Downloads/tentativa_letrass/obj_train_data'
+
+# Process the text files and create the histogram
+number_counts = process_text_files(directory)
+create_histogram(number_counts)
