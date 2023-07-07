317ad2b59d024a5147b8e98af5bc8931f1265cb0
santitblm <95491383+santitblm@users.noreply.github.com>
2023-07-07 11:37:33 +0100

    Corrected write characters
    and updated annotations

--- RandomCodigos/XML/Write_characters.py
+++ RandomCodigos/XML/Write_characters.py
@@ -3,7 +3,7 @@ import cv2
 import xml.etree.ElementTree as ET
 
 # Path to the XML file
-xml_file = "C:\\Users\\Vastingood\\Documents\\GitHub\\Tese\\RandomCodigos\\XML\\annotations.xml"
+xml_file = "C:/Users/Vastingood/Documents/GitHub/Tese/RandomCodigos/XML/annotations.xml"
 
 # Path to the images folder
 images_folder = "C:/Users/Vastingood/Documents/GitHub/Tese/cropped/"
@@ -25,18 +25,23 @@ for image in root.findall('image'):
         image_path = os.path.join(images_folder, image_name)
         img = cv2.imread(image_path)
 
+        # Get the polygons with label 'A'
+        polygons = [polygon for polygon in image.findall('polygon') if polygon.get('label') == 'A']
+
+        # Sort the polygons based on the x-coordinate of their first point
+        sorted_polygons = sorted(polygons, key=lambda polygon: float(polygon.get('points').split(';')[0].split(',')[0]))
+
         # Display the image
         cv2.imshow('Image', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        # Prompt the user to input 6 characters
-        user_input = input("Enter 6 characters: ")
+        # Prompt the user to input characters
+        user_input = input("Enter characters: ")
 
-        # Update each polygon label with the user input
-        for polygon in image.findall('polygon'):
-            polygon.set('label', user_input[0])
-            user_input = user_input[1:]  # Move to the next character
+        # Update the labels based on the order of the polygons
+        for polygon, char in zip(sorted_polygons, user_input):
+            polygon.set('label', char)
 
         # Save the updated XML file
         tree.write(xml_file)
