84342e70f0c83635372d3cec6282473d3a0836e5
santitblm <95491383+santitblm@users.noreply.github.com>
2023-07-10 10:32:12 +0100

    Create for_homo.py

---
+++ RandomCodigos/data_augmentation/for_homo.py
@@ -0,0 +1,53 @@
+
+'EHMNXZ' # quadrilátero
+'CGOS'   # redondo letras
+'03689'  # redondo numeros
+
+#sem irmaos
+
+'12457'
+
+'ADFJKLPQRTUV'
+
+quadrilatero = [(0, 0), (0, 1), (1, 1), (1, 0)]
+redondo_letras = [(0, 0), (0, 1), (1, 1), (1, 0)]
+redondo_numeros = [(0, 0), (0, 1), (1, 1), (1, 0)]
+
+points_dict = {
+    'A': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'B': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'C': redondo_letras,
+    'D': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'E': quadrilatero,
+    'F': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'G': redondo_letras,
+    'H': quadrilatero,
+    'I': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'J': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'K': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'L': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'M': quadrilatero,
+    'N': quadrilatero,
+    'O': redondo_letras,
+    'P': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'Q': redondo_letras,
+    'R': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'S': redondo_letras,
+    'T': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'U': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'V': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    'X': quadrilatero,
+    'Z': quadrilatero,
+
+    '0': redondo_numeros,
+    '1': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    '2': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    '3': redondo_numeros,
+    '4': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    '5': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    '6': redondo_numeros,
+    '7': [(0, 0), (0, 1), (1, 1), (1, 0)],
+    '8': redondo_numeros,
+    '9': redondo_numeros,
+    
+}
