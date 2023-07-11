7b5967a0e40ae97e84dfedd9bb05cc1d0186a946
santitblm <95491383+santitblm@users.noreply.github.com>
2023-07-11 16:50:07 +0100

    Update Class_distribution.py

--- RandomCodigos/XML/Class_distribution.py
+++ RandomCodigos/XML/Class_distribution.py
@@ -1,6 +1,6 @@
-import xml.etree.ElementTree as ET
+import os
 import matplotlib.pyplot as plt
-import string
+import xml.etree.ElementTree as ET
 
 # Path to the XML file
 xml_file = "C:/Users/Santi LM/Documents/GitHub/Tese/RandomCodigos/XML/annotations.xml"
@@ -9,10 +9,12 @@ xml_file = "C:/Users/Santi LM/Documents/GitHub/Tese/RandomCodigos/XML/annotation
 tree = ET.parse(xml_file)
 root = tree.getroot()
 
-# Count the occurrences of each polygon label
+# Dictionary to store label counts
 label_counts = {}
 
+# Iterate over each image element
 for image in root.findall('image'):
+    # Iterate over each polygon element
     for polygon in image.findall('polygon'):
         label = polygon.get('label')
         if label in label_counts:
@@ -20,27 +22,46 @@ for image in root.findall('image'):
         else:
             label_counts[label] = 1
 
-# Generate all letter labels from A to Z
-letters = list(string.ascii_uppercase)
+# Separate labels into letters and numbers
+letters = []
+numbers = []
 
-# Generate all number labels from 0 to 9
-numbers = [str(i) for i in range(10)]
+for label, count in label_counts.items():
+    if label.isalpha():
+        letters.append((label, count))
+    elif label.isdigit():
+        numbers.append((label, count))
 
-# Prepare data for plotting
-x_labels = range(len(letters + numbers))
-y_labels = [label_counts.get(label, 0) for label in letters + numbers]
+# Sort the letters and numbers in alphabetical and numerical order respectively
+letters = sorted(letters, key=lambda x: x[0])
+numbers = sorted(numbers, key=lambda x: int(x[0]))
 
-# Plot the histogram
-plt.figure(figsize=(10, 6))
-plt.bar(x_labels[:26], y_labels[:26], color='blue', label='Letters')
-plt.bar(x_labels[26:], y_labels[26:], color='red', label='Numbers')
+# Extract labels and counts for plotting
+letter_labels, letter_counts = zip(*letters)
+number_labels, number_counts = zip(*numbers)
+
+# Plotting the histogram
+fig, ax = plt.subplots()
+letter_bars = ax.bar(range(len(letter_labels)), letter_counts, color='blue')
+number_bars = ax.bar(range(len(letter_labels), len(letter_labels) + len(number_labels)), number_counts, color='red')
 
 # Customize the plot
-plt.xlabel('Label')
-plt.ylabel('Count')
-plt.title('Occurrences of Polygon Labels')
-plt.xticks(list(x_labels), letters + numbers)
-plt.legend()
+ax.set_xlabel('Labels')
+ax.set_ylabel('Count')
+ax.set_xticks(range(len(letter_labels + number_labels)))
+ax.set_xticklabels(letter_labels + number_labels)
+ax.legend((letter_bars, number_bars), ('Letters', 'Numbers'))
+ax.set_title('Label Histogram')
 
+# Print the sum of each column
+print("Sum of each column:")
+print("Letters:")
+for label, count in letters:
+    print(label, ":", count)
+print("Total:", sum(letter_counts))
+for label, count in numbers:
+    print(label, ":", count)
+print("Total:", sum(number_counts))
+print("Grand Total:", sum(letter_counts) + sum(number_counts))
 # Show the plot
 plt.show()
