5e52e1fe9b17e7e8a8f7a72e5f68d85308fb1682
santitblm <95491383+santitblm@users.noreply.github.com>
2023-08-14 15:26:54 +0100

    mais updates

--- RandomCodigos/data_augmentation/display_binary+polygon_TEST-ONLY.py
+++ RandomCodigos/data_augmentation/display_binary+polygon_TEST-ONLY.py
@@ -22,36 +22,46 @@ root = tree.getroot()
 # Iterate over each normal image
 for image in root.findall('image'):
     image_id = image.get('id')
-    image_name = image.get('name')
+    image_name = "transformed_" + image.get('name')
 
-    # Load the normal image
-    image_path = os.path.join(images_folder, image_name)
-    img = cv2.imread(image_path)
-
-    # Load the corresponding binary image
+    # Check if the binary image exists
     binary_image_path = os.path.join(binary_images_folder, image_name)
-    binary_img = cv2.imread(binary_image_path, cv2.IMREAD_GRAYSCALE)
+    if os.path.exists(binary_image_path):
+        # Load the normal image
+        image_path = os.path.join(images_folder, image_name)
+        img = cv2.imread(image_path)
 
-    # Create a mask where the binary image is black
-    mask = (binary_img == 0)
+        # Load the corresponding binary image
+        gray_img = cv2.imread(binary_image_path, cv2.IMREAD_GRAYSCALE)
+        _, binary_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # Create a mask where the binary image is black
+        mask = (binary_img == 0)
 
-    # Iterate over each polygon except 'LP'
-    polygons = image.findall("polygon[@label!='LP']")
-    for polygon in polygons:
-        points_str = polygon.get('points').split(';')
-        points = np.array([list(map(float, point.split(','))) for point in points_str], dtype=np.int32)
+        # Create a mask for polygons
+        polygons_mask = np.zeros_like(binary_img)
 
-        # Fill the polygon in the mask with white color
-        cv2.fillPoly(mask, [points], 255)
+        # Iterate over each polygon except 'LP'
+        polygons = image.findall("polygon[@label!='LP']")
+        for polygon in polygons:
+            points_str = polygon.get('points').split(';')
+            points = np.array([list(map(float, point.split(','))) for point in points_str], dtype=np.int32)
 
-    # Apply the mask to the normal image
-    result_img = cv2.bitwise_and(img, img, mask=mask)
+            # Fill the polygon in the mask
+            cv2.fillPoly(polygons_mask, [points], 255)
 
-    # Set non-masked pixels to white
-    result_img[~mask] = [255, 255, 255]
+        # Combine the masks
+        final_mask = np.logical_and(mask, polygons_mask)
 
-    # Display the resulting image
-    cv2.imshow("Resulting Image", result_img)
-    cv2.waitKey(0)
+        # Set pixels outside the mask to black
+        #result_img = np.zeros_like(img)
+        #result_img[final_mask] = img[final_mask]
+
+        # Set pixels outside the mask to white
+        result_img = np.copy(img)
+        result_img[~final_mask] = [255, 255, 255]
+
+        # Display the resulting image
+        cv2.imshow("Resulting Image", result_img)
+        cv2.waitKey(0)
 
 cv2.destroyAllWindows()
