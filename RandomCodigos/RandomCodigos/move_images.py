dc2ab023f956a253acec0d21c46aca113f3181a9
santitblm <95491383+santitblm@users.noreply.github.com>
2023-09-14 11:09:20 +0100

    Update move_images.py

--- RandomCodigos/RandomCodigos/move_images.py
+++ RandomCodigos/RandomCodigos/move_images.py
@@ -10,14 +10,13 @@ dir_d = "/home/santilm/Desktop/Tese/datasets/PT_LP_Characters/test_nolabels/imag
 # Get the list of files in directories B and C
 files_b = os.listdir(dir_b)
 files_c = os.listdir(dir_c)
-
 # Iterate through files in directory A
 for filename in os.listdir(dir_a):
     file_path_a = os.path.join(dir_a, filename)
-    
-    # Check if the file exists in directories B and C
-    if filename not in files_b and filename not in files_c:
-        # Copy the file from A to D
-        shutil.copy(file_path_a, os.path.join(dir_d, filename))
+    if filename.endswith(('.jpg', '.jpeg', '.png')):
+        # Check if the file exists in directories B and C
+        if filename not in files_b and filename not in files_c:
+            # Copy the file from A to D
+            shutil.copy(file_path_a, os.path.join(dir_d, filename))
 
 print("Copying completed.")
