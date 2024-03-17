d32211e8ba14f6b48a27df70a2135889706ffd52
santitblm <95491383+santitblm@users.noreply.github.com>
2024-03-17 11:03:27 +0000

    changed treinar License_Plates

--- Backups/treinarLPs.py
+++ Backups/treinarLPs.py
@@ -1,14 +1,38 @@
 from ultralytics import YOLO
- 
-# Load the model.
-model = YOLO('yolov8n.pt')
- 
-# Training.
-results = model.train(
-   data='datasets/License_Plates/License_Plate.yaml',
-   imgsz=1280,
-   epochs=200,
-   batch=8,
-   name='yolov8s_200e_1280',
-   patience = 5
-)
+
+imgsize = 1280
+
+models = ["s", "n"]
+
+for size in models: 
+   # Load the model.
+   model = YOLO(f'yolov8{size}.pt')
+   
+   # Training.
+   results = model.train(
+      data='/home/planeamusafrente/Desktop/SANTI/Tese/datasets/License_Plates/License_Plate.yaml',
+      imgsz=1280,
+      epochs=200,
+      batch=16,
+      name=f'License_Plates_{imgsize}_{size}',
+      patience = 50,
+      close_mosaic = 100
+   )
+
+
+models = ["l", "m"]
+
+for size in models: 
+   # Load the model.
+   model = YOLO(f'yolov8{size}.pt')
+   
+   # Training.
+   results = model.train(
+      data='/home/planeamusafrente/Desktop/SANTI/Tese/datasets/License_Plates/License_Plate.yaml',
+      imgsz=imgsize,
+      epochs=300,
+      batch=16,
+      name=f'License_Plates_{imgsize}_{size}',
+      patience = 50,
+      close_mosaic = 150
+   )
--- RandomCodigos/outros/remove_imgs_without_txtlabels.py
+++ RandomCodigos/outros/remove_imgs_without_txtlabels.py
@@ -3,38 +3,27 @@ import cv2
 
 #TXT_ROOT = "/home/santilm/Desktop/Tese/datasets/PT_LP_Characters/train/labels/"
 #IMG_ROOT = "/home/santilm/Desktop/Tese/datasets/PT_LP_Characters/train/images/"
-TXT_ROOT = "/home/santilm/Desktop/labels/"
-IMG_ROOT = "/home/santilm/Desktop/imagens para LPs dataset/"
+TXT_ROOT = "/home/santilm/Desktop/Tese/datasets/License_Plates/train/labels/"
+IMG_ROOT = "/home/santilm/Desktop/Tese/datasets/License_Plates/train/images/"
 
-all_txt_files = os.listdir(TXT_ROOT)
-
-#zero_counter = 0
-#txt_without_obj = []
-#for i, file_name in enumerate(all_txt_files):
-#    file_path = os.path.join(TXT_ROOT, file_name)
-#    with open(file_path, 'r') as f:
-#        lines = f.readlines()
-#        if len(lines) == 0:
-#            zero_counter += 1
-#            txt_without_obj.append(file_name.split('.txt')[0])
-#    f.close()
 
 all_img_files = os.listdir(IMG_ROOT)
-new_label_counter  = 0
+removed_image_counter  = 0
 for i, file_name in enumerate(all_img_files):
     file_path = os.path.join(IMG_ROOT, file_name)
-    #print(file_path)
     name = os.path.join(TXT_ROOT, '.'.join(file_name.split('.')[:-1])+'.txt')
-    print(name)
+    #print(name)
     if not os.path.isfile(name):
-        new_label_counter += 1
-        with open(name, 'w') as f:
-            f.write("")
+        removed_image_counter += 1
+        #with open(name, 'w') as f:
+        #    f.write("")
+        
         # image = cv2.imread(os.path.join(IMG_ROOT, file_name))
         # cv2.imshow('Image', image)
         # cv2.waitKey(0)
+
         #os.remove(file_path)
 
         #os.remove(os.path.join(TXT_ROOT, '.'.join(file_name.split('.')[:-1])+'.txt'))
 
-print(new_label_counter)
+print(removed_image_counter)
