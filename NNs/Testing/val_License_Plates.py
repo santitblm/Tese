903fb76f2fcc5a431087d62cd79a4aae9fe236ac
santitblm <95491383+santitblm@users.noreply.github.com>
2024-04-03 21:58:42 +0100

    Update val_License_Plates.py

--- NNs/Testing/val_License_Plates.py
+++ NNs/Testing/val_License_Plates.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("/home/santilm/Documents/GitHub/Tese/runs/detect/License_Plates_1280_x/weights/best.pt")
+#username, first_path = "santilm", "santilm/Desktop"
+username, first_path = "planeamusafrente", "planeamusafrente/Desktop/SANTI"
 
-metrics = model.val(data = "/home/santilm/Desktop/Tese/datasets/License_Plates/License_Plates_val.yaml")
+model = YOLO(f"/home/{username}/Documents/GitHub/Tese/runs/detect/License_Plates_1280_x/weights/best.pt")
+
+metrics = model.val(data = f"/home/{first_path}/Tese/datasets/License_Plates/License_Plates_val.yaml")
 
 print(metrics.box)
