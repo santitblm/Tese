5c9fb9c90b1c284c39ff175ee90d4399a827d8ea
santitblm <95491383+santitblm@users.noreply.github.com>
2024-04-05 18:15:40 +0100

    added all validation folders
    changed a few other things

--- NNs/Testing/val_License_Plates.py
+++ NNs/Testing/val_License_Plates.py
@@ -3,8 +3,37 @@ from ultralytics import YOLO
 #username, first_path = "santilm", "santilm/Desktop"
 username, first_path = "planeamusafrente", "planeamusafrente/Desktop/SANTI"
 
-model = YOLO(f"/home/{username}/Documents/GitHub/Tese/runs/detect/License_Plates_1280_x/weights/best.pt")
+sizes = ["n", "s", "m"]#, "l", "x"]
 
-metrics = model.val(data = f"/home/{first_path}/Tese/datasets/License_Plates/License_Plates_val.yaml")
+models = ["License_Plates_1280", "LP_fromCars_480", "PT_LP_Characters"]
+vals = ["License_Plates", "LPs_fromCars", "PT_LP_Characters"]
 
-print(metrics.box)
+sizes = ["l"]
+
+models = ["License_Plates_1280"]
+vals = ["License_Plates"]
+
+
+#models = ["LP_fromCars_480"]
+#vals = ["LPs_fromCars"]
+
+
+
+for model, val in zip(models, vals):
+    for size in sizes:
+        system = f"{model}_{size}"
+        #try:
+        print(f"MODEL BEING TESTED: {system}")
+
+        net = YOLO(f"/home/{username}/Documents/GitHub/Tese/runs/detect/{system}/weights/best.pt")
+        
+        metrics = net.val(data = f"/home/{first_path}/Tese/datasets/{val}/{val}_val.yaml", 
+                            name = f"VAL_{system}",
+                            exist_ok = True
+                            , batch = 15
+                            )
+        #except:
+            #print(system, "needs to be looked at\n")
+        print(f"{(metrics.box.class_result(0)[2])*100:.2f} & {(metrics.box.class_result(0)[3])*100:.2f} & {(metrics.box.class_result(1)[2])*100:.2f} & {(metrics.box.class_result(1)[3])*100:.2f} & {(metrics.box.map50)*100:.2f} & {(metrics.box.map)*100:.2f}")
+
+#print(metrics.box)
--- NNs/Testing/val_PT_LP_Characters.py
+++ NNs/Testing/val_PT_LP_Characters.py
@@ -1,5 +1,60 @@
 from ultralytics import YOLO
 
-model = YOLO("/home/santilm/Documents/GitHub/Tese/runs/detect/PT_LP_Characters_m/weights/best.pt")
+class_labels = "ABCDEFGHIJKLMNOPQRSTUVXZ0123456789"
 
-model.val(data = "/home/santilm/Desktop/Tese/datasets/PT_LP_Characters/PT_LP_Characters_val.yaml")
+#username, first_path = "santilm", "santilm/Desktop"
+username, first_path = "planeamusafrente", "planeamusafrente/Desktop/SANTI"
+
+sizes = ["n", "s", "m", "l", "x"]
+
+models = ["PT_LP_Characters"]
+vals = ["PT_LP_Characters"]
+
+
+#models = ["LP_fromCars_480"]
+#vals = ["LPs_fromCars"]
+
+
+
+for model, val in zip(models, vals):
+    for size in sizes:
+        system = f"{model}_{size}"
+        #try:
+        print(f"MODEL BEING TESTED: {system}")
+
+        net = YOLO(f"/home/{username}/Documents/GitHub/Tese/runs/detect/{system}/weights/best.pt")
+        
+        metrics = net.val(data = f"/home/{first_path}/Tese/datasets/{val}/{val}_val.yaml", 
+                            name = f"VAL_{system}",
+                            exist_ok = True
+                            #, batch = 15
+                            )
+        #except:
+            #print(system, "needs to be looked at\n")
+        
+        map50_worstClass = 100
+        map50_95_worstClass = 100
+        map50_bestClass = 0
+        map50_95_bestClass = 0
+
+        for i in range(34):
+            map50_newValue = metrics.box.class_result(i)[2]
+            map50_95_newValue = metrics.box.class_result(i)[3]
+            # Check if the new value is smaller than the current worst value
+            if map50_newValue < map50_worstClass:
+                map50_worstClass = map50_newValue
+                map50_95_worstClass = map50_95_newValue
+                worst_class = class_labels[i]
+
+            # Check if the new value is bigger than the current best value
+            elif map50_newValue > map50_bestClass:
+                map50_bestClass = map50_newValue
+                map50_95_bestClass = map50_95_newValue
+                best_class = class_labels[i]
+            
+
+
+
+        print(f"({best_class}, {worst_class})      & {(map50_bestClass)*100:.2f} & {(map50_95_bestClass)*100:.2f} & {(map50_worstClass)*100:.2f} & {(map50_95_worstClass)*100:.2f} & {(metrics.box.map50)*100:.2f} & {(metrics.box.map)*100:.2f}\n")
+
+#print(metrics.box)
